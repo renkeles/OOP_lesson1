7c10c70033a39083e07a83140db075b40ce27a34
renkeles <84261162+renkeles@users.noreply.github.com>
2021-08-24 12:31:25 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdint>
 
 class Power {
 private:
@@ -14,6 +15,25 @@ public:
     }
 };
 
+class RGBA {
+private:
+    std::uint8_t m_red   = 0;
+    std::uint8_t m_green = 0;
+    std::uint8_t m_blue  = 0;
+    std::uint8_t m_alpha = 255;
+public:
+    RGBA(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha) :
+        m_red(red), m_green(green), m_blue(blue), m_alpha(alpha) { 
+    }
+    void print() {
+        //std::cout.operator<<(m_red);
+        std::cout << "red = " << (int)m_red << std::endl;
+        std::cout << "green = " << (int)m_green << std::endl;
+        std::cout << "blue = " << (int)m_blue << std::endl;
+        std::cout << "alpha = " << (int)m_alpha << std::endl;
+    }
+};
+
 int main()
 {
     Power first;
@@ -21,6 +41,10 @@ int main()
     Power second;
     second.set(2.0, 3.3);
     second.calculate();
+
+
+    RGBA rgba(12,175,43,250);
+    rgba.print();
     
 
 
